a79cdc4cd6503a8d4f5e56add0cfd676d6c776d9
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-01-14 12:42:01 -0300

    the models were created

---
+++ app/Models/user_people_group.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class user_people_group extends Model
+{
+    use HasFactory;
+}
