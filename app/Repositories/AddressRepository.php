f7fa49c5a856c79a99a005a238f5ba798d8ffecf
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-01-16 19:56:34 -0300

    Repository Classes  were created

---
+++ app/Repositories/AddressRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Address;
+
+
+class AddressRepository
+{
+
+ 
+}
---
+++ app/Repositories/BuildingRepairRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BuildingRepair;
+
+
+class BuildingRepairRepository
+{
+
+ 
+}
+
