525cf22d81d521ab17c4eba5fbe443462daea7da
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-02-25 21:32:25 -0300

    object request was added in folder services

--- front/src/services/request.js
+++ front/src/services/request.js
@@ -1,22 +1,64 @@
-
-export function httpRequest(method,url,body=null,header={'Content-Type':'application/json'}) {
-    fetch(url,{
-        method:method,
-        header:header,
-        body:body
-    });
-}
-
-export function POST(url,body,header=false){   
-    header?  httpRequest("POST",url,body):httpRequest("POST",url,body,header)
-}
-export function PUT(url,body,header=false){   
-    header?  httpRequest("PUT",url,body):httpRequest("POST",url,body,header)
-}
-export function DELTE(url,body,header=false){   
-    header?  httpRequest("DELETE",url,body):httpRequest("POST",url,body,header)
-}
-export function GET(url,body,header=false){   
-    header?  httpRequest("GET",url,body):httpRequest("POST",url,body,header)
-}
-
+async function JSONHttpRequest(method,url,body) {
+    const response = await fetch(url,{
+         method:method,
+         headers:{'Content-Type':'application/json'},
+         body:JSON.stringify(body)
+     });
+     return response;
+ }
+ 
+ async function tryRequest(method,url,bodyContent,callBack){
+     const response = await JSONHttpRequest(method, url, bodyContent);
+     const data = await response.json();
+     return callBack? callBack(data): data;
+ }
+ 
+ const request={
+     
+  post: async(url,bodyContent,callBack=false)=>{
+     try{
+         const response = await tryRequest("POST",url,bodyContent,callBack);
+         return response;
+     } catch (error) {
+         console.error(error);
+     };
+  },
+ 
+  put: async (url,bodyContent,callBack=false)=>{   
+     try{
+         const response = await tryRequest("PUT",url,bodyContent,callBack);
+         return response;
+     } catch (error) {
+         console.error(error);
+     };
+  },
+ 
+  patch: async(url,bodyContent,callBack=false)=>{   
+     try{
+         const response = await tryRequest("PATCH",url,bodyContent,callBack);
+         return response;
+     } catch (error) {
+         console.error(error);
+     };
+  },
+ 
+  get: async(url,callBack)=>{
+     try {
+         const response = await fetch(url);
+         const data = await response.json();
+         callBack(data);
+     } catch (error) {
+         console.error(error);
+     };
+ },
+ 
+  delete: async(url,callBack)=>{
+     try {
+         const response = await fetch(url, {method: 'DELETE',});
+         const data = await response.json();
+         callBack(data);
+     } catch (error) {
+         console.error(error);
+     };
+  },
+};
